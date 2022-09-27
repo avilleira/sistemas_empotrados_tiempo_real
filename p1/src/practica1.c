d4ecd48cec707ed27416d22f966f6d1b843b00f7
Jaime Avilleira <j.avilleira.2019@alumnos.urjc.es>
2022-09-27 10:10:05 +0200

    Threads created, they work.
    Signed-off-by: Jaime Avilleira <j.avilleira.2019@alumnos.urjc.es>

--- p1/src/practica1.c
+++ p1/src/practica1.c
@@ -6,18 +6,26 @@
 #define MAX_THREADS 4
 
 void *print_message_function(void *ptr) {
-    fprintf(stderr, "Hi! I'm fn\n");
+    char *msg;
+    msg = (char *) ptr;
+    for (int j = 0; j <= MAX_THREADS; j++) {
+        fprintf(stdout, "%s - Iteracion %d: Coste=0.50s\n", msg, j+1);
+    }
     return NULL;
 }
+
+
 int main(int argc, char *argv[]) {
 
     int i;
+    char **threads_names = {"Thread 1", "Thread 2", "Thread 3", "Thread 4"};
     pthread_t threads[MAX_THREADS];
 
     for (i = 0; i < MAX_THREADS; i++) {
-        pthread_create(&threads[i], NULL, print_message_function, NULL);
+        pthread_create(&threads[i], NULL, print_message_function, (void*) threads_names[i]);
     }
     fprintf(stderr, "HI, IM  MAIN\n");
+
     for (i = 0; i < MAX_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
