6b8373852e92ce18135e8217fb4e5988dbdfa57b
Jaime Avilleira <j.avilleira.2019@alumnos.urjc.es>
2022-09-29 09:04:10 +0200

    Working on the thread function, making again the time conversion algorithm
    Signed-off-by: Jaime Avilleira <j.avilleira.2019@alumnos.urjc.es>

--- p1/src/practica1.c
+++ p1/src/practica1.c
@@ -5,12 +5,22 @@
 
 #define MAX_THREADS 4
 
-void *print_message_function(void *ptr) {
+void *thread_function(void *ptr) {
+    long double time_before, time_after;
+    struct timespec begin;
+    char *time_str = malloc(sizeof(begin.tv_sec)*2);
+    clock_gettime(CLOCK_REALTIME, &begin);
+    sprintf(time_str, "%ld.%ld", begin.tv_sec, begin.tv_nsec);
     char *msg;
     msg = (char *) ptr;
+    printf("[%s\n]", time_str);
+    time_before = (long double)atof(time_str);
+    printf("[%Lf\n]", time_before);
     for (int j = 0; j <= MAX_THREADS; j++) {
         fprintf(stdout, "%s - Iteracion %d: Coste=0.50s\n", msg, j+1);
     }
+
+    free(time_str);
     return NULL;
 }
 
@@ -18,11 +28,11 @@ void *print_message_function(void *ptr) {
 int main(int argc, char *argv[]) {
 
     int i;
-    char **threads_names = {"Thread 1", "Thread 2", "Thread 3", "Thread 4"};
+    char *threads_names[] = {"Thread 1", "Thread 2", "Thread 3", "Thread 4"};
     pthread_t threads[MAX_THREADS];
 
     for (i = 0; i < MAX_THREADS; i++) {
-        pthread_create(&threads[i], NULL, print_message_function, (void*) threads_names[i]);
+        pthread_create(&threads[i], NULL, thread_function, (void*) threads_names[i]);
     }
     fprintf(stderr, "HI, IM  MAIN\n");
 
